f378be69c92610d0f1f1b110d4da4cd47d304d80
brian gatundu <brianwgatundu@gmail.com>
2025-04-08 23:23:47 +0300

    add vercel speed and analytics components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <SpeedInsights />
+    <Analytics />
     <TooltipProvider>
       <Toaster />
       <Sonner />
